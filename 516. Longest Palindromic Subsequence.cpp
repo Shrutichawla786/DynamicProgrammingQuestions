cf9c89e9ade1243dc2fe578fdf11ef099ee96b0e
shruti chawla <114687114+Shrutichawla786@users.noreply.github.com>
2024-03-18 20:51:14 +0530

    516. Longest Palindromic Subsequence

---
+++ 516. Longest Palindromic Subsequence.cpp
@@ -0,0 +1,26 @@
+Q.516. Longest Palindromic Subsequence
+link-->https://leetcode.com/problems/longest-palindromic-subsequence/submissions/1207314723/
+code-->class Solution {
+public:
+int solve(int i1, int i2 ,string &s1, string &s2 , vector<vector<int>>&dp){
+        if(i1<0 || i2<0){
+            return 0;
+        }
+        if(dp[i1][i2]!=-1){
+            return dp[i1][i2];
+        }
+        if(s1[i1]==s2[i2]){
+            return 1+solve(i1-1 , i2-1 , s1 , s2 , dp);
+        }
+        else{
+            return dp[i1][i2]= max(solve(i1-1 , i2 , s1 ,s2 , dp) , solve(i1,i2-1 , s1 , s2 , dp));
+        }
+    }
+    int longestPalindromeSubseq(string s) {
+        string t= s;
+        reverse(t.begin() , t.end());
+        int n = s.length();
+        vector<vector<int>>dp(n , vector<int>(n+1 , -1));
+        return solve(n-1 , n-1 ,s , t , dp);
+    }
+};
