03210bdeb6a0567b4044a37182a6a80496942ac5
shruti chawla <114687114+Shrutichawla786@users.noreply.github.com>
2024-05-17 15:13:55 +0530

    Boolean Evaluation

---
+++ Boolean Evaluation.cpp
@@ -0,0 +1,58 @@
+Q. Boolean Evaluation
+code-->int mod=1000000007;
+long long f(int i , int j ,int isTrue, string & exp , vector<vector<vector<long long>>>&dp){
+    if(i>j){
+        return 0;
+    }
+    if(i==j){
+        if(isTrue==1){
+            return (exp[i]=='T');
+        }
+        else{
+            return (exp[i]=='F');
+        }
+    }
+    if(dp[i][j][isTrue]!=-1){
+        return dp[i][j][isTrue];
+    }
+    long long ways=0;
+    for(int ind=i+1;ind<=j-1 ;ind+=2){
+        long long lT= f(i , ind-1 , 1 , exp , dp);
+        long long lF= f(i , ind-1 , 0 , exp ,dp);
+        long long rT= f(ind+1 , j , 1 , exp,dp);
+        long long rF= f(ind+1 , j , 0 , exp,dp);
+    
+
+    if(exp[ind]=='&'){
+        if(isTrue){
+            ways=(ways+ (lT*rT)%mod)%mod;
+        }
+        else{
+            ways=( ways+ (lT*rF)%mod + (lF*rT)%mod + (lF*rF)%mod)%mod;
+        }
+        
+    }
+    else if(exp[ind]=='|'){
+        if(isTrue){
+            ways=( ways+ (lT*rF)%mod + (lF*rT)%mod + (lT*rT)%mod)%mod;
+        }
+        else{
+            ways= (ways+(lF*rF)%mod)%mod;
+        }
+    }
+    else if(exp[ind]=='^'){
+        if(isTrue){
+             ways=( ways+ (lT*rF)%mod + (lF*rT)%mod )%mod;
+        } else {
+             ways = (ways + (lT * rT) % mod + (lF * rF) % mod) % mod;
+        }
+    }
+    }
+    return ways%mod;
+}
+int evaluateExp(string & exp) {
+    // Write your code here.
+    int n =exp.size();
+    vector<vector<vector<long long>>>dp(n , vector<vector<long long>>(n , vector<long long>(n , -1) ));
+    return f(0 ,exp.size()-1 , 1, exp ,dp);
+}
